d1b541eb185cdfa4c8ca01f5724f89f582d08fc4
Clint Simon <clint@clintsimon.com>
2021-03-12 12:48:08 -0800

    Add commandline args

--- src/Bruno/Bruno/Program.cs
+++ src/Bruno/Bruno/Program.cs
@@ -1,12 +1,42 @@
 ﻿namespace Bruno
 {
     using System;
+    using CommandLine;
 
     internal class Program
     {
-        public static void Main(string[] args)
+        private Program(Options options)
         {
-            Console.WriteLine("Hello World!");
+            _options = options;
+        }
+
+        private readonly Options _options;
+
+        public static void Main(string[] args)
+            => Parser
+               .Default
+               .ParseArguments<Options>(args)
+               .WithParsed(options => new Program(options).Execute());
+
+        private void Execute()
+        {
+            if (_options.Verbose)
+            {
+                Console.WriteLine($"Verbose output enabled. Current Arguments: -v {_options.Verbose}");
+                Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+            }
+            else
+            {
+                Console.WriteLine($"Current Arguments: -v {_options.Verbose}");
+                Console.WriteLine("Quick Start Example!");
+            }
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
         }
     }
 }
